91d4f6fa533e174db6cca151493e8ebb42a48e2b
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2026-08-31 19:38:03 +0700

    chore: remove astro cache folder

--- .astro/env.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'astro:env/client' {
-	export const SHOW_UPCOMING_LESSONS: boolean;	
-}
--- .astro/fonts.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'astro:assets' {
-	/** @internal */
-	export type CssVariable = (["--font-lato","--font-inconsolata"])[number];
-}
